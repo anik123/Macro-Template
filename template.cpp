b4a7c6e3e1b5b6f694f61df3b0483f6ccc7edc71
Anik Islam Abhi <anik123@users.noreply.github.com>
2016-09-21 11:22:00 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,59 @@
+#include<cstdio>
+#include<sstream>
+#include<cstdlib>
+#include<cctype>
+#include<cmath>
+#include<algorithm>
+#include<set>
+#include<queue>
+#include<stack>
+#include<list>
+#include<iostream>
+#include<fstream>
+#include<numeric>
+#include<string>
+#include<vector>
+#include<cstring>
+#include<map>
+#include<iterator>
+#include<bitset>
+using namespace std;
+#define sf scanf
+#define pf printf
+#define re return
+#define i64 __int64
+#define I64 %I64d
+#define u64 unsigned __int64
+#define U64 "%U64d"
+#define ll long long
+#define forit(i, m) for (i=(m).begin(); i!=(m).end(); ++i)
+#define rab(i,a,b) for(i=a;i<=b;i++)
+#define ras(i,a,b) for(i=a;i>=b;i--)
+#define rep(i,n) rab(i,0,n-1)
+#define fi(n) rep(i,n)
+#define fj(n) rep(j,n)
+#define fk(n) rep(k,n)
+#define max(a,b) a>b? a:b
+#define min(a,b) a<b? a:b
+#define eps (1e-11)
+#define eq(a,b) ((a-b) < eps && (b-a) < eps)
+#define less(a,b) (b-a>eps)
+#define inf (1<<30)
+#define pi 2*acos(0.0)
+#define Set(t,v) memset((t), (v), sizeof(t))
+#define pb push_back
+#define sz(v) (v).size()
+#define vt vector<int>
+#define mp make_pair
+#define all(x) x.begin(),x.end()
+#define rev(x) reverse(all(x))
+#define im int main()
+#define sq(a) (a)*(a)
+#define max 31
+ll gcd(ll a,ll b){while(b){b ^=a ^=b ^=a %=b;}return a;}
+im
+{    
+    
+   // write your code here
+re 0;
+}
